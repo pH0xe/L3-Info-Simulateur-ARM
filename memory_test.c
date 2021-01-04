60807e2550a88b64145bb36b7656d6dd6e176a93
Julien Reig <reig.julien@free.fr>
2021-01-04 08:43:01 +0100

    Partie 1 : memory (#1)
    * memory.c complété
    
    *  test validé
    
    * prise en compte de size

--- memory_test.c
+++ memory_test.c
@@ -1,24 +1,24 @@
 /*
-Armator - simulateur de jeu d'instruction ARMv5T à but pédagogique
+Armator - simulateur de jeu d'instruction ARMv5T ï¿½ but pï¿½dagogique
 Copyright (C) 2011 Guillaume Huard
 Ce programme est libre, vous pouvez le redistribuer et/ou le modifier selon les
-termes de la Licence Publique Générale GNU publiée par la Free Software
-Foundation (version 2 ou bien toute autre version ultérieure choisie par vous).
+termes de la Licence Publique Gï¿½nï¿½rale GNU publiï¿½e par la Free Software
+Foundation (version 2 ou bien toute autre version ultï¿½rieure choisie par vous).
 
-Ce programme est distribué car potentiellement utile, mais SANS AUCUNE
+Ce programme est distribuï¿½ car potentiellement utile, mais SANS AUCUNE
 GARANTIE, ni explicite ni implicite, y compris les garanties de
-commercialisation ou d'adaptation dans un but spécifique. Reportez-vous à la
-Licence Publique Générale GNU pour plus de détails.
+commercialisation ou d'adaptation dans un but spï¿½cifique. Reportez-vous ï¿½ la
+Licence Publique Gï¿½nï¿½rale GNU pour plus de dï¿½tails.
 
-Vous devez avoir reçu une copie de la Licence Publique Générale GNU en même
-temps que ce programme ; si ce n'est pas le cas, écrivez à la Free Software
+Vous devez avoir reï¿½u une copie de la Licence Publique Gï¿½nï¿½rale GNU en mï¿½me
+temps que ce programme ; si ce n'est pas le cas, ï¿½crivez ï¿½ la Free Software
 Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307,
-États-Unis.
+ï¿½tats-Unis.
 
 Contact: Guillaume.Huard@imag.fr
-	 Bâtiment IMAG
+	 Bï¿½timent IMAG
 	 700 avenue centrale, domaine universitaire
-	 38401 Saint Martin d'Hères
+	 38401 Saint Martin d'Hï¿½res
 */
 #include <stdio.h>
 #include <stdlib.h>
@@ -65,6 +65,58 @@ int compare_with_sim(void *a, memory m, size_t size, int reverse) {
             return 0;
     }
     return 1;
+}
+
+void test64() {
+    memory m[2];
+    uint32_t word_value = 0x11223344, word_read;
+    uint16_t half_value = 0x5566, half_read;
+    uint8_t *position;
+    uint64_t tinquiete = 0x123456789ABCDEF;
+    int i;
+
+    m[1] = memory_create(8,1);
+    m[0] = memory_create(8,0);
+    if ((m[1] == NULL) || (m[0] == NULL)) {
+        fprintf(stderr, "Error when creating simulated memory\n");
+        exit(1);
+    }
+
+    printf("Writing 8 bytes at address 0, then reading the word and half, "
+           "the result should depend on simulated memory endianess :\n");
+    position = (uint8_t *) & tinquiete;
+    for (i=0; i<8; i++) {
+        memory_write_byte(m[0], i, *(position+i));
+        memory_write_byte(m[1], i, *(position+i));
+    }
+    printf("- word read with the same endianess as me, ");
+    memory_read_word(m[is_big_endian()], 0, &word_read);
+    print_test(compare(&word_value, &word_read, 4, 0));
+    printf("- half read with the same endianess as me, ");
+    memory_read_half(m[is_big_endian()], 0, &half_read);
+    print_test(compare(&word_value, &half_read, 2, 0));
+    printf("- word read with a different endianess than me, ");
+    memory_read_word(m[1-is_big_endian()], 0, &word_read);
+    print_test(compare(&word_value, &word_read, 4, 1));
+    printf("- half read with a different endianess than me, ");
+    memory_read_half(m[1-is_big_endian()], 0, &half_read);
+    print_test(compare(&word_value, &half_read, 2, 1));
+
+    printf("Writing word and half at address 0, then reading the bytes, "
+           "the result should depend on simulated memory endianess :\n");
+    printf("- word write with the same endianess as me, ");
+    memory_write_word(m[is_big_endian()], 0, word_value);
+    print_test(compare_with_sim(&word_value, m[is_big_endian()], 4, 0));
+    printf("- half write with the same endianess as me, ");
+    memory_write_half(m[is_big_endian()], 0, half_value);
+    print_test(compare_with_sim(&half_value, m[is_big_endian()], 2, 0));
+    printf("- word write with a different endianess than me, ");
+    memory_write_word(m[1-is_big_endian()], 0, word_value);
+    print_test(compare_with_sim(&word_value, m[1-is_big_endian()], 4, 1));
+    printf("- half write with a different endianess than me, ");
+    memory_write_half(m[1-is_big_endian()], 0, half_value);
+    print_test(compare_with_sim(&half_value, m[1-is_big_endian()], 2, 1));
+
 }
 
 int main() {
@@ -118,6 +170,9 @@ int main() {
     printf("- half write with a different endianess than me, ");
     memory_write_half(m[1-is_big_endian()], 0, half_value);
     print_test(compare_with_sim(&half_value, m[1-is_big_endian()], 2, 1));
+
+    printf("\n\n");
+    test64();
 
     return 0;
 }
