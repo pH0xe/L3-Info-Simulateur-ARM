9be5560f90608e041df9d39c893abb00f8fabe4b
NielasAran0 <morand.lucas@yahoo.fr>
2021-01-11 15:12:02 +0100

    Ouais les fichiers ouais

---
+++ Examples/test1.s
@@ -0,0 +1,15 @@
+.global main
+.text
+main:
+    ldr r2,=donnees
+    mov r1,#0
+loop:
+    ldrb r0,[r2, r1]
+    eors r0,r0,#0xF0
+    add r1,r1,#1
+    bne loop
+    swi 0x123456
+.data
+donnees:
+    .word 0x12345678
+    .word 0x9ABCDEF0
